830577d57f051ab795cf849c53bb3e73e4c190ed
Manav Gagvani <mgagvani@outlook.com>
2024-02-14 14:26:22 -0500

    vesc control w/ arduino and laptop/vesc comms

---
+++ Arduino_VESC_Control/Arduino_VESC_Control.ino
@@ -0,0 +1,33 @@
+#include <Servo.h>
+#include <Arduino.h>
+
+const uint8_t VescOutputPin = 5;
+
+
+Servo esc;
+
+void setup() {
+    esc.attach(VescOutputPin);
+    esc.writeMicroseconds(1500); // Send "stop" signal to VESC
+    Serial.begin(115200);
+    Serial.begin(115200);
+    Serial.println("VESC Control");
+    delay(1900);
+}
+
+ void loop() {
+
+  // ramp up to 0.5 throttle
+    for (int i = 1500; i <= 1900; i+=100) {
+        Serial.println(i);
+        esc.writeMicroseconds(i);
+        delay(1000);
+    }
+
+    // ramp down to stop
+    for (int i = 1900; i >= 1500; i-=100) {
+        Serial.println(i);
+        esc.writeMicroseconds(i);
+        delay(1000);
+    }
+}
