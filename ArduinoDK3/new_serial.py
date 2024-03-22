12e9db0b847731675e89b8896a47846b409f8777
Manav Gagvani <mgagvani@outlook.com>
2024-03-22 13:57:25 -0400

    tests, more journal reports

--- ArduinoDK3/new_serial.py
+++ ArduinoDK3/new_serial.py
@@ -1,7 +1,9 @@
 import serial as serial
+import time
 
 ser = serial.Serial('COM11', 115200, timeout=1)
 
+'''
 while True:
     list_ = [int(x) for x in input("steer throttle: ").split()]
 
@@ -9,3 +11,18 @@ while True:
     # just like over serial console terminal
     msg = f"<hello, {list_[0]}, {list_[1]}>"
     ser.write(msg.encode())
+'''
+while True:
+    for i in range(1000, 2000, 50):
+        ser.read(ser.in_waiting)
+        ser.write(f"<hello, {i}, 1500>".encode())
+        ser.flush()
+        time.sleep(0.1)
+        print(i)
+
+    for i in range(2000, 1000, -50):
+        ser.read(ser.in_waiting)
+        ser.write(f"<hello, {i}, 1500>".encode())
+        ser.flush()
+        time.sleep(0.1)
+        print(i)
